bb9d8390db23688617775d21906200dcdbd8c7a9
Dennis Kimaita <dkimaita22@gmail.com>
2023-10-04 18:10:00 +0300

    create hero table in models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Hero(db.Model):
+    __tablename__ = 'hero'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+# add any models you may need. 
